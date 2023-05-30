60481c74b357e4d4e7551a334a72d44ae858a369
guguidfr <guguidfr@protonmail.com>
2023-05-30 20:43:22 +0200

    :alien: It worked once

--- Development/app/stop.sh
+++ Development/app/stop.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
 read -p "Do you want to stop all running containers?[Y/N]: " option
-if [ $option == "Y" || $option == "y" ]
+if [[ $option == "Y" || $option == "y" ]]
 then
 
     echo "Stopping all containers..."
     docker stop $(docker ps -q)
 
     read -p "Do you want to remove all containers?[Y/N]: " selection
-    if [ $selection == "Y" || $selection == "y" ]
+    if [[ $selection == "Y" || $selection == "y" ]]
     then
         docker rm $(docker ps -aq)
-    elif [ $selection == "N" || $selection == "n" ]
+    elif [[ $selection == "N" || $selection == "n" ]]
     then
         echo "..."
     fi
 
-elif [ $option == "N" || $option == "N" ]
+elif [[ $option == "N" || $option == "N" ]]
 then
     echo "..."
 else
     echo "Invalid option"
     exit 1
+fi
