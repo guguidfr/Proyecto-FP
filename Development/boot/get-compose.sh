f7f24339ae2cf31eb2f6dbcab572ab8c53d6ca8c
guguidfr <guguidfr@protonmail.com>
2023-06-09 20:11:11 +0200

    Changes in typos/interpreter

--- Development/boot/get-compose.sh
+++ Development/boot/get-compose.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 sudo curl -L "https://github.com/docker/compose/releases/download/v2.2.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/bin/docker-compose
 sudo chmod +x /usr/bin/docker-compose
 sudo apt install -y uidmap
