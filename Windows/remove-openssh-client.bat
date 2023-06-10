c23c07f650fe181eb92843bef340b086586b4243
guguidfr <guguidfr@protonmail.com>
2023-06-10 13:33:02 +0200

    Fixed .bat file

--- Windows/remove-openssh-client.bat
+++ Windows/remove-openssh-client.bat
@@ -1,6 +1,3 @@
 @echo off
-echo "This .bat file uninstalls the OpenSSH Client of Windows."
-echo "If you want to proceed, press ENTER; use Ctrl+C to exit this script."
-rem Remember that if you don't remove OpenSSH, you will need to pass your ssh public key to the Vagrant VM.
-pause >nul
+echo This .bat file uninstalls the OpenSSH Client of Windows.
 dism /online /Remove-Capability /CapabilityName:OpenSSH.Client~~~~0.0.1.0
